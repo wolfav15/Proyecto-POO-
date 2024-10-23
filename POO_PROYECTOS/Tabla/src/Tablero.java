6e088e6172621f89fe92b6e4471c24695de820b9
wolfav15 <130191198+wolfav15@users.noreply.github.com>
2024-10-22 21:41:10 -0300

    Update Tablero.java

--- POO_PROYECTOS/Tabla/src/Tablero.java
+++ POO_PROYECTOS/Tabla/src/Tablero.java
@@ -1,11 +1,29 @@
 
 public class Tablero {
-private int idTablero;
-private String tipoTablero;
+	private Jugador jugador1;
+	private Jugador jugador2; 
+        private int idTablero;
+        private String tipoTablero;
 
 public Tabla (int idTabla, String tipoTabla) {
 	this.idTablero= idTabla;
 	this.tipoTablero =tipoTabla; 
 }
-
+      public static List<Tablero> obtenerTableros(Connection connection) throws SQLException {
+    List<Tablero> tableros = new ArrayList<>();
+    String sql = "SELECT * FROM tableros";
+    try (PreparedStatement stmt = connection.prepareStatement(sql);
+         ResultSet rs = stmt.executeQuery()) {
+        while (rs.next()) {
+            Tablero tablero = new Tablero(
+                rs.getInt("tablero_id"),
+                rs.getString("tipo_tablero")
+            );
+            tableros.add(tablero);
+        }
+    }
+    return tableros;
+}
+}
+	
 }
