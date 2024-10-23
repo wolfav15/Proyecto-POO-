2191f7d6234b6fef11b3caf9d47a9f821ff35d77
wolfav15 <130191198+wolfav15@users.noreply.github.com>
2024-10-22 21:38:36 -0300

    Add files via upload

---
+++ POO_PROYECTOS/Tabla/src/Tablero.java
@@ -0,0 +1,11 @@
+
+public class Tablero {
+private int idTablero;
+private String tipoTablero;
+
+public Tabla (int idTabla, String tipoTabla) {
+	this.idTablero= idTabla;
+	this.tipoTablero =tipoTabla; 
+}
+
+}
