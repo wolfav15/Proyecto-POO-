ae933dbe852902df40817f9c605ca17475c3db00
BrandonC <chiguay.brandond@gmail.com>
2024-10-28 14:44:19 -0300

    Eliminacion cementerio jugador

--- Main/src/Jugador/Jugador.java
+++ Main/src/Jugador/Jugador.java
@@ -2,13 +2,13 @@ package Jugador;
 
 import java.util.List;
 import Clases.*;
-//No tengo idea de como jugar Yu-Gi-Oh :D
+
 public class Jugador {
     private String nombre;
     private int puntosVida = 8000;
     private List <Carta> deck;
     private List <Carta> mano;
-    private List <Carta> cementerio;
+    //private List <Carta> cementerio; //No se usará por el momento
     
     public Jugador(String nombre, List <Carta> deck, List <Carta> mano, List <Carta> cementerio) {
         this.nombre = nombre;
@@ -21,7 +21,8 @@ public class Jugador {
         this.mano.add(this.deck.remove(0));
     } 
 
-    public void jugarCarta(Carta cartaJugada) {
+    public void jugarCarta(Carta cartaJugada) { //Este metodo no se usará por el momento
+        
         
     }
 
