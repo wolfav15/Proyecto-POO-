33d07b22052460a802312f7d1d95774db7616eac
BrandonC <chiguay.brandond@gmail.com>
2024-10-30 20:21:53 -0300

    Primeros avances jugarCarta()

--- Main/src/Jugador/Jugador.java
+++ Main/src/Jugador/Jugador.java
@@ -8,23 +8,52 @@ public class Jugador {
     private int puntosVida = 8000;
     private List <Carta> deck;
     private List <Carta> mano;
+    private Campo campoJugador;
+
     //private List <Carta> cementerio; //No se usará por el momento
     
-    public Jugador(String nombre, List <Carta> deck, List <Carta> mano, List <Carta> cementerio) {
+    public Jugador(String nombre, List <Carta> deck) {
         this.nombre = nombre;
         this.deck = deck;
-        this.mano = mano;
-        this.cementerio = cementerio;
+        //this.cementerio = cementerio;
     }
 
     public void robarCarta() {
         this.mano.add(this.deck.remove(0));
     } 
 
-    public void jugarCarta(Carta cartaJugada) { //Este metodo no se usará por el momento
+    public void jugarCarta(Carta cartaJugada) { 
+        String jugada =    cartaJugada.getClass().toString();
+
+
+        switch (jugada) {
+            case "CartaMonstruo":
+                this.campoJugador.agregarMonstruo(cartaJugada);
+                break;
+                
+            case "CartaMagicaEquipo":
+                this.campoJugador.agregarCartaMagicaEquipo(cartaJugada);
+                break;
+                
+            case "CartaMagicaNormal":
+                    this.campoJugador.agregarCartaMagica(cartaJugada);
+                    cartaJugada.activar_efecto();
+                break;
+                
         
-        
+            default:
+                break;
+        }
+
+        if (jugada.equals("CartaMonstruo")) {
+            this.campoJugador.agregarMonstruo(cartaJugada);
+         }
     }
+
+    public void robarManoInicial()  {
+        for (int i = 0; i < 5; i++) {
+            this.robarCarta();
+    } }
 
     public void recibirDaño(int puntos) {
         this.puntosVida -= puntos;
